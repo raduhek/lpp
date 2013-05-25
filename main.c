a4ec00bb48c71acaa8cfc51b1d2372be9b71c1b5
Cruceru Radu <radu.cruceru@gmail.com>
2013-05-26 01:33:49 +0300

    removed a lot of stuff from main.c

--- main.c
+++ main.c
@@ -2,154 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "stack.h"
+#include "stack/stack.h"
+#include "parse_tree/utils.h"
+#include "parse_tree/parse_tree.h"
 
 #define INPUT_FILE "phrases"
-
-typedef struct parantheses_match_struct {
-    int start;
-    int end;
-} pair_t;
-
-typedef struct parse_tree_struct {
-    void *val;
-    struct parse_tree_struct *children[2];
-} parse_tree_t;
-
-parse_tree_t *new_parse_tree (void *val, parse_tree_t *left, parse_tree_t *right) {
-    parse_tree_t *t = (parse_tree_t*) malloc (sizeof(parse_tree_t));
-
-    t->val = val;
-    t->children[0] = left;
-    t->children[1] = right;
-
-    return t;
-}
-
-char *string_copy(char *source, int from, int to, char replacer) {
-    int start, end, idx;
-    char *ret;
-
-    start = from + 1;
-    while (source[start] == '\t' || source[start] == ' ') {
-        start++;
-    }
-
-    end = to - 1;
-    while ((source[end] == '\t' || source[end] == ' ') && end >= start) {
-        end--;
-    }
-    
-    if (end <= start) {
-        source[from] = source[to] = ' ';
-        return NULL;
-    }
-
-    ret = (char*) malloc((end - start + 2) * sizeof(char));
-    ret[end - start + 1] = '\0';
-    
-    for (idx = end; idx >= start; --idx) {
-        ret[idx - start] = source[idx];
-        if (replacer) {
-            source[idx] = replacer;
-        }
-    }
-    if (replacer) {
-        source[from] = source[to] = replacer;
-    }
-
-    return ret;
-}
-
-char *surround_with_parantheses(char *str, int len) {
-    int i;
-    char *ret = (char *) malloc((len + 4) * sizeof(char));
-    ret[0] = '(';
-    for (i = 0; i <= len; i++) {
-        ret[i + 1] = str[i];
-    }
-    ret[len + 2] = ')';
-    ret[len + 3] = '\0';
-
-    return ret;
-}
-
-char *check_parantheses(char *str, stack_t *pairs_stack) {
-    stack_t *temp_stack = new_stack();
-    pair_t *pair;
-    pair_t *last_pair;
-    int ok = 1;
-    int i = 0;
-    int start, end;
-    int enclose = 1;
-    int offset = 0;
-
-    char *return_string;
-
-    while (str[i]) {
-        if (str[i] == '(') {
-            pair = (pair_t*) malloc(sizeof(pair_t));
-            pair->start = i;
-            stack_push(temp_stack, (void*)pair);
-        }
-        if (str[i] == ')') {
-            pair = (pair_t*) stack_pop(temp_stack);
-            if (pair == NULL) {
-                while ((pair = (pair_t*)stack_pop(pairs_stack)) != NULL) {
-                    free(pair);
-                }
-                free(temp_stack);
-                return 0;
-            }
-
-            pair->end = i;
-            stack_push(pairs_stack, (void*)pair);
-        }
-        i++;
-    }
-
-    while ((pair = (pair_t*) stack_pop(temp_stack)) != NULL) {
-        free(pair);
-        ok = 0;
-    }
-
-    if (! ok ) {
-        return 0;
-    }
-    start = 0;
-    end = i - 1;
-
-    while ((pair = (pair_t*) stack_peak (pairs_stack)) != NULL) {
-        //printf("data: %d %d\n", start, end);
-        //printf("pair: %d %d\n", pair->start, pair->end);
-        if (pair->start == start && pair->end == end) {
-            last_pair = (pair_t*) stack_pop (pairs_stack);
-            enclose = 0;
-            start ++;
-            end --;
-        } else {
-            break;
-        }
-    }
-
-    offset = start;
-
-    if (enclose == 0) {
-        stack_push (pairs_stack, (void*) last_pair);
-        return_string = (char *) malloc((last_pair->end - last_pair->start + 1) * sizeof(char));
-        return_string[last_pair->end - last_pair->start] = '\0';
-        for (i = last_pair->end; i >= last_pair->start ; --i) {
-            return_string[i - last_pair->start ] = str[i];
-        }
-    } else {
-        return_string = surround_with_parantheses(str, end);
-        pair = (pair_t*) malloc(sizeof(pair_t));
-        pair->start = offset;
-        pair->end = end + offset;
-        stack_push (pairs_stack, (void*)pair);
-    }
-    return return_string;
-}
 
 parse_tree_t *construct_parse_tree(char *str, stack_t *brackets) {
     int start;
@@ -185,7 +42,7 @@ parse_tree_t *construct_parse_tree(char *str, stack_t *brackets) {
             ok = 0;
             operator_position = inner_pair->end;
             for (idx = inner_pair->start; idx <= inner_pair->end; ++idx) {
-                if (str[idx] == '&' || str[idx] == '|') {
+                if (str[idx] == '>' || str[idx] == '&' || str[idx] == '|') {
                     operator_position = idx;
                     ok ++;
                     operator = (char*) malloc(2 * sizeof(char));
@@ -245,7 +102,7 @@ parse_tree_t *construct_parse_tree(char *str, stack_t *brackets) {
             ok = 0;
             operator_position = outer_pair->end;
             for (idx = outer_pair->start; idx <= outer_pair->end; ++idx) {
-                if (str[idx] == '&' || str[idx] == '|') {
+                if (str[idx] == '>' || str[idx] == '&' || str[idx] == '|') {
                     operator_position = idx;
                     ok ++;
                     operator = (char*) malloc(2 * sizeof(char));
@@ -452,9 +309,9 @@ int main(int argc, char **argv) {
         }*/
         phrase = remove_whitespaces(line);
         if (phrase) {
-            phrase = check_parantheses(phrase, phrase_parantheses_stack);
+            phrase = sanitize_phrase(phrase, phrase_parantheses_stack);
             empty_stack(phrase_parantheses_stack);
-            phrase = check_parantheses(phrase, phrase_parantheses_stack);
+            phrase = sanitize_phrase(phrase, phrase_parantheses_stack);
             root = construct_parse_tree(phrase, phrase_parantheses_stack);
             traverse_parse_tree(root, 0);
             printf("\n\n");
