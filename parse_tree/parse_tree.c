3657dd6c6d2ccd3f26567cae41c9fdae93b1b75a
Cruceru Radu <radu.cruceru@gmail.com>
2013-06-05 04:31:44 +0300

    refactored create_tree (pretty much rewrote it) and other stuff in parse_tree

--- parse_tree/parse_tree.c
+++ parse_tree/parse_tree.c
@@ -2,170 +2,144 @@
 #include "stdio.h"
 
 #include "../stack/stack.h"
+#include "../sanitizer/sanitizer.h"
+#include "../pair/pair.h"
 #include "parse_tree.h"
 #include "utils.h"
 
-parse_tree_t *new_parse_tree (void *val, parse_tree_t *left, parse_tree_t *right) {
+parse_tree_t *new_parse_tree (void *val, parse_tree_t *left, parse_tree_t *right, short int is_operator) {
     parse_tree_t *t = (parse_tree_t*) malloc (sizeof(parse_tree_t));
 
     t->val = val;
-    t->children[0] = left;
-    t->children[1] = right;
+    t->left = left;
+    t->right = right;
+    t->is_operator = is_operator;
 
     return t;
 }
 
-parse_tree_t *construct_parse_tree(char *str, stack_t *brackets) {
-    int start;
-    int end;
-    int idx;
-    int ok;
-    char *last_token;
-    char *operator ;
-    int operator_position;
+/*
+ * construct_tree will return a parse tree for a given formula
+ *
+ * It receives a formula which is assumed is sanitized and all
+ * and a stack of parentheses pairs
+ * Both arguments will be altered after this function returns:
+ *  - the stack will be empty
+ */
+parse_tree_t *construct_tree(char *str, stack_t *brackets) {
+    // root will be returned
+    parse_tree_t *root;
+
+    // These are used internally to construct the tree
+    parse_tree_t *left, *right;
+
+    // These are used internally to handle parentheses
+    pair_t *outer_pair, *inner_pair, *current_pair;
+
+    // This is used to put the parentheses pair in it from the brackets stack
+    // and then are popped and processed
+    stack_t *inner_pair_stack = new_stack();
+
+    // This stack is used to construct the tree
     stack_t *children_stack = new_stack();
     
-    parse_tree_t *root, *left, *right;
+    char *token;
 
-    stack_t *brackets_batch = new_stack();
-    pair_t *pair;
-    pair_t *outer_pair;
-    pair_t *inner_pair;
-
-    // Get the first pair from the stack since it contains the entire phrase
-    outer_pair = (pair_t*) stack_pop(brackets);
-    start = outer_pair->start;
-    end = outer_pair->end;
-
-    while ((pair = (pair_t*) stack_peak(brackets)) != NULL) {
-        while ((inner_pair = (pair_t*) stack_peak(brackets)) && 
-                    (inner_pair->start > start) && (inner_pair->end < end)) {
+    // Get 
+    while (NULL != (outer_pair = (pair_t*)stack_peak(brackets))) {
+        outer_pair = (pair_t*) stack_pop(brackets);
+        stack_push (inner_pair_stack, (void*) outer_pair);
+        while ((NULL != (inner_pair = (pair_t*) stack_peak(brackets))) &&
+                                inner_pair->start > outer_pair->start &&
+                                inner_pair->end < outer_pair->end) {
 
             inner_pair = (pair_t*) stack_pop(brackets);
-            stack_push(brackets_batch, (void*) inner_pair);
+            stack_push (inner_pair_stack, (void *) inner_pair);
         }
 
-        while ((inner_pair = (pair_t*) stack_pop(brackets_batch)) != NULL) {
-            ok = 0;
-            operator_position = inner_pair->end;
-            for (idx = inner_pair->start; idx <= inner_pair->end; ++idx) {
-                if (str[idx] == '>' || str[idx] == '&' || str[idx] == '|') {
-                    operator_position = idx;
-                    ok ++;
-                    operator = (char*) malloc(2 * sizeof(char));
-                    *operator = str[idx];
-                    *(operator + 1) = '\0';
-                    str[idx] = ' ';
-                }
-            }
+        while (NULL != (current_pair = (pair_t*) stack_pop (inner_pair_stack))) {
+            /*
+             * We have a few cases to handle here
+             */
+            left = NULL;
+            right = NULL;
+            str[current_pair->start] = '[';
+            str[current_pair->end] = ']';
+            // This is the case of a single token in parentheses (maybe negated)
+            if (-1 == current_pair->operator_position) {
+                token = string_copy(str, current_pair->start + 1, current_pair->end - 1);
 
-            if (ok > 1) {
-                empty_stack(brackets_batch);
-                // free_parse_tree(&root);
-                printf("Error inserting: %s\nMust have at most one binary operator. found : %d\n", str, ok);
-                exit(11);
-            }
-
-            start = inner_pair->start;
-            end = operator_position;
-            do {
-                last_token = string_copy(str, start, end, ' ');
-                if (last_token != NULL) {
-                    root = new_parse_tree((void*)last_token, NULL, NULL);
-
-                    stack_push(children_stack, (void*) root);
-                }
-                start = end;
-                end = inner_pair->end;
-            } while (start != inner_pair->end);
-
-            if (operator_position != inner_pair->end) {
-                right = (parse_tree_t*) stack_pop(children_stack);
-                left = (parse_tree_t*) stack_pop(children_stack);
-
-                if (left == NULL) {
-                    printf("Error: left token not found\n");
-                    exit(14);
+                // It's negated :D
+                if (token[0] == '~') {
+                    token++;
+                    
+                    if (token[0] == '[') {
+                        left = (parse_tree_t*) stack_pop(children_stack);
+                    } else {
+                        unencode_string(token);
+                        left = new_parse_tree((void *)token, NULL, NULL, 0);
+                    }
+                    root = new_parse_tree((void *)'~', left, NULL, 1);
+                } else {
+                    unencode_string(token);
+                    root = new_parse_tree((void *)token, NULL, NULL, 0);
                 }
 
-                if (right == NULL) {
-                    printf("Error: right token not found\n");
-                    exit(14);
+                stack_push(children_stack, (void*)root);
+            } else {
+                // Here we have to check whether we have processed a pair (by checking for [ or ])
+                // or maybe a negation sign is somewhere
+                // Process left side
+                if (str[current_pair->start + 1] == '~') {
+                    // The left part is negated
+                    if (str[current_pair->start + 2] == '[') {
+                        // We have processed this pair, so just get the node
+                        left = (parse_tree_t*) stack_pop(children_stack);
+                    } else {
+                        token = string_copy(str, current_pair->start + 2, current_pair->operator_position - 1);
+                        unencode_string(token);
+                        left = new_parse_tree((void *) token, NULL, NULL, 0);
+                    }
+                    root = new_parse_tree((void*) '~', left, NULL, 1);
+                    left = root;
+                } else {
+                    // The left part is not negated
+                    if (str[current_pair->start + 1] == '[') {
+                        left = (parse_tree_t*) stack_pop(children_stack);
+                    } else {
+                        token = string_copy(str, current_pair->start + 1, current_pair->operator_position - 1);
+                        unencode_string(token);
+                        left = new_parse_tree((void *) token, NULL, NULL, 0);
+                    }
                 }
 
-                root = new_parse_tree((void*)operator, left, right);
-                stack_push (children_stack, (void*) root);
+                // Process right side
+                if (str[current_pair->operator_position + 1] == '~') {
+                    // The right part is negated
+                    if (str[current_pair->operator_position + 2] == '[') {
+                        // We have processed this pair, so just get the node
+                        right = (parse_tree_t*) stack_pop(children_stack);
+                    } else {
+                        token = string_copy(str, current_pair->operator_position + 2, current_pair->end - 1);
+                        right = new_parse_tree((void *) token, NULL, NULL, 0);
+                    }
+                    root = new_parse_tree((void*) '~', right, NULL, 1);
+                    right = root;
+                } else {
+                    // The right part is not negated
+                    if (str[current_pair->operator_position + 1] == '[') {
+                        right = (parse_tree_t*) stack_pop(children_stack);
+                    } else {
+                        token = string_copy(str, current_pair->operator_position + 1, current_pair->end - 1);
+                        right = new_parse_tree((void *) token, NULL, NULL, 0);
+                    }
+                }
+
+                root = new_parse_tree((void*) str[current_pair->operator_position], left, right, 1);
+                stack_push(children_stack, (void *) root);
             }
         }
-
     }
-
-    /*
-
-        Handling outer most parantheses pair
-
-    */
-
-            ok = 0;
-            operator_position = outer_pair->end;
-            for (idx = outer_pair->start; idx <= outer_pair->end; ++idx) {
-                if (str[idx] == '>' || str[idx] == '&' || str[idx] == '|') {
-                    operator_position = idx;
-                    ok ++;
-                    operator = (char*) malloc(2 * sizeof(char));
-                    *operator = str[idx];
-                    *(operator + 1) = '\0';
-                    str[idx] = ' ';
-                }
-            }
-
-            if (ok > 1) {
-                empty_stack(brackets_batch);
-                // free_parse_tree(&root);
-                printf("Error inserting: %s\nMust have at most one binary operator. found : %d\n", str, ok);
-                exit(11);
-            }
-
-            start = outer_pair->start;
-            end = operator_position;
-            do {
-                last_token = string_copy(str, start, end, ' ');
-                if (last_token != NULL) {
-                    root = new_parse_tree((void*)last_token, NULL, NULL);
-
-                    stack_push(children_stack, (void*) root);
-                }
-                start = end;
-                end = outer_pair->end;
-            } while (start != outer_pair->end);
-
-            if (operator_position != outer_pair->end) {
-                
-                right = (parse_tree_t*) stack_pop(children_stack);
-                left = (parse_tree_t*) stack_pop(children_stack);
-
-                if (left == NULL) {
-                    printf("Error: left token not found\n");
-                    exit(14);
-                }
-
-                if (right == NULL) {
-                    printf("Error: right token not found\n");
-                    exit(14);
-                }
-
-                root = new_parse_tree((void*)operator, left, right);
-                stack_push (children_stack, (void*) root);
-            }
-
-
-    root = (parse_tree_t*) stack_pop (children_stack);
-
-    if (root == NULL) {
-        printf("Error: parse tree root vanished :-??\n");
-        exit(14);
-    }
-
-    return root;
+    return (parse_tree_t*)stack_pop(children_stack);
 }
 
--- parse_tree/parse_tree.h
+++ parse_tree/parse_tree.h
@@ -4,22 +4,16 @@
 #include "../stack/stack_node.h"
 
 /*
- * Forward declarations
- */
-typedef struct parantheses_match_struct {
-    int start;
-    int end;
-} pair_t;
-
-/*
  * Definition of parse_tree_struct
  */
 typedef struct parse_tree_struct {
     void *val;
-    struct parse_tree_struct *children[2];
+    struct parse_tree_struct *left;
+    struct parse_tree_struct *right;
+    short int is_operator;
 } parse_tree_t;
 
-parse_tree_t *new_parse_tree (void *, parse_tree_t *, parse_tree_t *);
-parse_tree_t *construct_parse_tree(char *, stack_t *); 
+parse_tree_t *new_parse_tree (void *, parse_tree_t *, parse_tree_t *, short int);
+parse_tree_t *construct_tree (char *, stack_t *); 
 #endif
 
